bbec275f74b35e2827819432390dbdc29aea9883
Keamogetswe Mfoloe <keamogetswemfoloe@gmail.com>
2022-12-25 14:08:24 +0200

    Fix: App crashing when receiving invalid params

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,78 +1,87 @@
-import path from "path";
-import express from "express";
+import path from 'path';
+import express from 'express';
 
-import resizeImage from "../utils/imageProcessor";
-import checkFileExtension, { checkDir, imageValidator } from "../utils/helpers";
+import resizeImage from '../utils/imageProcessor';
+import checkFileExtension, { checkDir, imageValidator } from '../utils/helpers';
 
 const routes = express.Router();
 
 routes.get(
-  "/",
-  async (req: express.Request, res: express.Response): Promise<unknown> => {
-    const filename: string = req.query.filename as string;
-    const width: number = parseInt(req.query.width as string);
-    const height: number = parseInt(req.query.height as string);
+    '/',
+    async (req: express.Request, res: express.Response): Promise<unknown> => {
+        const filename: string = req.query.filename as string;
+        const width: number = parseInt(req.query.width as string);
+        const height: number = parseInt(req.query.height as string);
 
-    console.log(filename, width, height);
-    imageValidator(filename, width, height); // check if the user entered all the parameters and if the width and height are numbers
+        console.log(filename, width, height);
+        if (imageValidator(filename, width, height) === false)
+            // check if the user entered all the parameters and if the width and height are numbers
+            return res
+                .status(400)
+                .send(
+                    'Invalid parameters! Check that the parameters height and width are positive numbers',
+                );
 
-    const originalImagePath = path.join(
-      __dirname,
-      "../../public/images",
-      `${filename}.jpg`
-    );
+        const originalImagePath = path.join(
+            __dirname,
+            '../../public/images',
+            `${filename}.jpg`,
+        );
 
-    /**
-     * check if the image the user wants to resize exists and if not send an error message
-     * @param originalImagePath The path of the original image
-     * @returns                 void if the image exists or error message if it doesn't
-     */
-    if (checkDir(originalImagePath) === false) {
-      return res.status(400).send("Image not found!");
-    }
+        /**
+         * check if the image the user wants to resize exists and if not send an error message
+         * @param originalImagePath The path of the original image
+         * @returns                 void if the image exists or error message if it doesn't
+         */
+        if (checkDir(originalImagePath) === false) {
+            return res.status(400).send('Image not found!');
+        }
 
-    const fileformat = path.extname(originalImagePath);
-    checkFileExtension(fileformat); // check if the file extension is valid
-    const resizedImagePath = path.join(
-      __dirname,
-      "../../public/images/thumbnail",
-      `${filename}-resized${width}x${height}+${fileformat}`
-    );
+        const fileformat = path.extname(originalImagePath);
+        if (checkFileExtension(fileformat) === false)
+            // check if the file extension is valid
+            return res.status(400).send('Invalid file extension!');
 
-    /**
-     * check if the image is already resized and if it is send the cached image
-     * @param resizedImagePath The path of the resized image
-     * @returns                void if the image is already resized
-     *
-     */
-    if (checkDir(resizedImagePath) === true) {
-      console.log("Image already resized!");
-      return res.status(200).sendFile(resizedImagePath); // send the cached image
-    }
+        const resizedImagePath = path.join(
+            __dirname,
+            '../../public/images/thumbnail',
+            `${filename}-resized${width}x${height}+${fileformat}`,
+        );
 
-    /**
-     * resize the image and send it to the user
-     * @param width            The width of the image
-     * @param height           The height of the image
-     * @param originalImagePath The path of the original image
-     * @param resizedImagePath  The path of the resized image
-     * @returns                 void if the image is resized successfully or Error if it fails to resize the image
-     */
-    try {
-      await resizeImage({
-        width,
-        height,
-        originalImagePath,
-        resizedImagePath,
-      });
-    } catch (error) {
-      return res
-        .status(400)
-        .send("Error occurred during processing the image!");
-    }
-    console.log("Image resized successfully!");
-    res.status(200).sendFile(resizedImagePath);
-  }
+        /**
+         * check if the image is already resized and if it is send the cached image
+         * @param resizedImagePath The path of the resized image
+         * @returns                void if the image is already resized
+         *
+         */
+        if (checkDir(resizedImagePath) === true) {
+            console.log('Image already resized!');
+            return res.status(200).sendFile(resizedImagePath); // send the cached image
+        }
+
+        /**
+         * resize the image and send it to the user
+         * @param width            The width of the image
+         * @param height           The height of the image
+         * @param originalImagePath The path of the original image
+         * @param resizedImagePath  The path of the resized image
+         * @returns                 void if the image is resized successfully or Error if it fails to resize the image
+         */
+        try {
+            await resizeImage({
+                width,
+                height,
+                originalImagePath,
+                resizedImagePath,
+            });
+        } catch (error) {
+            return res
+                .status(400)
+                .send('Error occurred during processing the image!');
+        }
+        console.log('Image resized successfully!');
+        res.status(200).sendFile(resizedImagePath);
+    },
 );
 
 export default routes;
